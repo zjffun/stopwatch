0356602e6cdc7f429f5142e8fa82b0ddd07b37c0
zhangjufeng <zjffun@gmail.com>
2020-05-22 19:04:10 +0800

    0.0.5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* stopwatch2 version 0.0.4 */
+/* stopwatch2 version 0.0.5 */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
