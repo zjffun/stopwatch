7c0d6814adf52523e65bb2e0d946b8da45360607
JuFeng Zhang <zjffun@gmail.com>
2020-08-26 20:03:25 +0800

    chore: update version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Stopwatch2 version 0.0.6 */
+/* Stopwatch2 version 0.0.7 */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
